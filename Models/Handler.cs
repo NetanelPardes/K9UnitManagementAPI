d1ef43f306b3abc2e145e44a4ab2d369627f51a5
Netanel Pardes <sendi8475@gmail.com>
2026-08-11 09:44:07 +0300

    Finishing the models

---
+++ Models/Handler.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.EntityFrameworkCore;
+
+namespace K9UnitManagementAPI.Models
+{
+    [Index(nameof(PersonalNumber), IsUnique = true)]
+    public class Handler
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "FullName is required")]
+        [StringLength(100, ErrorMessage = "FullName maximum characters is 100")]
+        public string FullName { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "PersonalNumber is required")]
+        [StringLength(10, ErrorMessage = "PersonalNumber maximum characters is 10")]
+        public string PersonalNumber { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Rank is required")]
+        [StringLength(30, ErrorMessage = "Rank maximum characters is 30")]
+        public string Rank { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "YearsOfExperience is required")]
+        [Range(0,40,ErrorMessage = "YearsOfExperience should be between 0 and 40")]
+        public int YearsOfExperience { get; set; }
+
+
+        [Required(ErrorMessage = "BaseAssigned is required")]
+        [StringLength(100, ErrorMessage = "BaseAssigned maximum characters is 100")]
+        public string BaseAssigned { get; set; } = string.Empty;
+    }
+}
---
+++ Models/TrainingSession.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.EntityFrameworkCore;
+
+namespace K9UnitManagementAPI.Models
+{
+    public class TrainingSession
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "SessionDate is required")]
+        public DateTime SessionDate { get; set; }
+
+        [Required(ErrorMessage = "DurationMinutes is required")]
+        [Range(1, 300, ErrorMessage = "DurationMinutes should be between 1 and 300")]
+        public int DurationMinutes { get; set; }
+
+        [Required(ErrorMessage = "TrainingType is required")]
+        [RegularExpression("^(Obedience|ScentDetection|Agility|FieldExercise|Endurance)$", ErrorMessage = "TrainingType must be one of the list")]
+        public string TrainingType { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "PerformanceScore is required")]
+        [Range(0, 100, ErrorMessage = "PerformanceScore should be between 0 and 100")]
+        public int PerformanceScore { get; set; }
+
+        public bool Passed { get; }
+
+        [Required(ErrorMessage = "Evaluator is required")]
+        [StringLength(100, ErrorMessage = "Evaluator maximum characters is 100")]
+        public string Evaluator { get; set; } = string.Empty;
+    }
+}
